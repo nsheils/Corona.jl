fba140157754b8b86c3ff838bd4845262729de56
Gabriele Camerlengo <camerlen@tnt.tu-berlin.de>
2020-04-14 22:26:13 +0200

    plot functions added

---
+++ src/plot.jl
@@ -0,0 +1,18 @@
+using Plots
+
+import Plots.plot
+function plot(da::DA,region::String,yaxis=:none::Symbol)
+    if yaxis == :log10
+        add = 1.0
+    else
+        add = 0.0
+    end
+    datatime = timestamp(da.data)[1:meta(da.data)["last_day_idxs"]]
+    scatter(da.data.Confirmed[datatime],color=:orange,
+                Atickfontsize=12,title=region,legend=:topleft,yaxis=yaxis)
+    scatter!(da.data.Deaths[datatime].+add,color=:black)
+    plot!(da.u.I .+ da.u.R .+ da.u.D,legend=:topleft,color=:orange,lw=3,label="C")
+    plot!(da.u.I,color=:red, lw=3)
+    plot!(da.u.R .+ add,color=:green, lw=3)
+    plot!(da.u.D .+ add,color=:black, lw=3)
+end
