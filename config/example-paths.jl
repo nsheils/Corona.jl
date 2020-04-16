ef31b80b87b77c33511c7fb627ecf384971ddd7a
Gabriele Camerlengo <camerlen@tnt.tu-berlin.de>
2020-04-16 16:08:57 +0200

    config/example-paths.jl added

---
+++ config/example-paths.jl
@@ -0,0 +1,8 @@
+##### Paths configuration file #####
+# Modify with your preferred paths and save as 'config/path.jl'
+
+# results path
+dataconfig.results_path = "results"
+
+# data path
+dataconfig.rawdata_path = "raw"
