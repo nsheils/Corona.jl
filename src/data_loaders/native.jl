3383c247827067fd5cdd7a962d0e55f5794c3474
Gabriele Camerlengo <camerlen@tnt.tu-berlin.de>
2020-04-16 16:39:44 +0200

    new native data format

--- src/data_loaders/native.jl
+++ src/data_loaders/native.jl
@@ -4,27 +4,13 @@ using DataFrames
 
 ###### native data loader #################
 
-
-_load_data(source::Val{:native}, region::AbstractString, path::AbstractString) = _load_data(source, (r".*", region), path)
-
-function _load_data(source::Val{:native}, region::Tuple{Union{AbstractString,Regex},AbstractString}, path::AbstractString)
-
-    fn = _build_filename(region[2])
+function _load_data(source::Val{:native}, region::AbstractString, path::AbstractString)
+    fn = _build_filename(region)
     fp = rstrip(path,'/')*"/"*fn*".csv"
-    if fn != region[2]
+    if fn != region
         @warn("loading data from `"*fp*"'")
     end
-
-    header = ["Date", "Region", "Confirmed", "Deaths"];
-    df = CSV.read(fp,header=header,copycols=true,dateformat="yyyy-mm-dd",ignoreemptylines=true)
-    df[ismissing.(df.Region),:Region] .= ""
-    df.Region = strip.(df.Region)
-    sub = occursin.(region[1],df.Region)
-    if any(sub)
-        cases = TimeArray(df.Date[sub],Array{Int64}(df[sub,3:4]),Symbol.(header[3:4]))
-    else
-        error("no entry in `$fp' for $region")
-    end
-
-    cases
+    header = ["Date", "Confirmed", "Deaths"];
+    df = CSV.read(fp,header=header,datarow=2,dateformat="yyyy-mm-dd",ignoreemptylines=true)
+    TimeArray(df.Date,Array{Int64}(df[!,2:3]),Symbol.(header[2:3]))
 end
