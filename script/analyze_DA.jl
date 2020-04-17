dcb44afd705de8468d2aa986fd8f7a45533b9bff
Gabriele Camerlengo <camerlen@tnt.tu-berlin.de>
2020-04-17 14:28:29 +0200

    better analyze_DA.jl

--- script/analyze_DA.jl
+++ script/analyze_DA.jl
@@ -8,13 +8,20 @@ using Formatting
 ### Load configuration
 dataconfig = Corona.DataConfig();
 include("../config/data.jl");
+if isfile(joinpath(Base.@__DIR__,"../config/paths.jl"))
+    include("../config/paths.jl")
+end;
+
+## Decide region and title
+region = "New York";
+title = "New York City";
 
 ## Select region and paths
-region = "New York";
 filename = Corona.build_filename(dataconfig,region);
 figdir = joinpath("figs",filename);
 mkpath(figdir);
-datapath = joinpath("results",filename,"da.jld2");
+datapath = joinpath(dataconfig.results_path,filename,"da.jld2");
+
 
 ## Load data
 da = load(datapath,"da");
@@ -25,26 +32,34 @@ J = Corona.residual(da, relative=true);
 
 ## Plot results
 plot(da,region)
-title!(format("King County, Washington, J = {:.3f} %",J*100))
+title!(format(title*", J = {:.3f} %",J*100))
+
+##
 savefig(joinpath(figdir,"da.pdf"))
 
 ## Plot results in log scale
 plot(da,region,yaxis=:log10,legend=:bottomright)
-title!(format("King County, Washington, J = {:.3f} %",J*100))
+title!(format(title*", J = {:.3f} %",J*100))
+
+##
 savefig(joinpath(figdir,"da_log.pdf"))
 
 ## Plot model parameters
 ptime = Date(2020,3,24):Day(1):lastday;
-plot(da.p.β[ptime],color=:red, lw=3, legend=false)
-scatter!(da.p.β[ptime],color=:red, lw=3)
+plot(da.p.β,color=:red, lw=3, legend=false)
+scatter!(da.p.β,color=:red, lw=3)
 ylabel!("beta")
-title!(format("King County, Washington, J = {:.3f} %",J*100))
+title!(format(title*", J = {:.3f} %",J*100))
+
+##
 savefig(joinpath(figdir,"beta.pdf"))
 
 ## Plot model parameters
 σ=da.p.β./(da.p.γ .+ da.p.δ);
-plot(σ[ptime],color=:orange, lw=3, legend=false)
-scatter!(σ[ptime],color=:orange, lw=3)
+plot(σ,color=:orange, lw=3, legend=false)
+scatter!(σ,color=:orange, lw=3)
 ylabel!("sigma")
-title!(format("King County, Washington, J = {:.3f} %",J*100))
+title!(format(title*", J = {:.3f} %",J*100))
+
+##
 savefig(joinpath(figdir,"sigma.pdf"))
