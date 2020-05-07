4bfd24a58b489d70d553b719c798dbd1559b2fe2
Gabriele Camerlengo <camerlen@tnt.tu-berlin.de>
2020-05-07 19:38:31 +0200

    latest run_DA.jl

--- script/run_DA.jl
+++ script/run_DA.jl
@@ -2,14 +2,16 @@ using Corona
 using Flux
 using Dates
 using LinearAlgebra
+using Interpolations
 using TimeSeries
 using Formatting
 
 ### Parameters
-maxiters     = 1000000;
+maxiters     = 100000;
 tolerance    = 0.001;
 screenfreq   = 10;
-filterfreq   = 1000000;
+# roffreq      = 100000;
+# rofparam     = 0.005;
 sponge       = 90;
 # opt          = Flux.Optimiser
 #                 ExpDecay(1e-6, 0.1, 1000, 1e-8),
@@ -17,11 +19,11 @@ sponge       = 90;
 #                 );
 opt          = Momentum(1e-3, 0.99);
 # opt          = NADAM(1e-12, (0.89, 0.995))
-coldstart    = false;
-#c₀           = Day(49);
-c₀           = missing;
-Δc           = Day(7);
-region       = "New York";
+coldstart    = true;
+c₀           = Day(0);
+#c₀           = missing;
+Δc           = Day(1000);
+region       = "Laputa";
 
 ############################################################
 printstyled("C O R O N A",bold=true,color=:blue)
@@ -55,25 +57,34 @@ else
 end;
 
 ### Create windows
+# if !ismissing(c₀)
+#    assimtime = data.lastdate-Δc:Day(1):data.lastdate;
+# else
+#     assimtime = data.outbreakdate+c₀:Day(1):min(data.lastdate,data.outbreakdate+c₀+Δc);
+# end
+# σ = TimeArray(assimtime,ones(length(assimtime),4),[:S,:I,:R,:D]);
+# μ = TimeArray(assimtime,ones(length(assimtime),3),[:β,:γ,:δ]);
+
 if ismissing(c₀)
-    assimtime = data.lastdate-Δc:Day(1):data.lastdate;
+    d₀ = data.lastdate
 else
-    assimtime = data.outbreakdate+c₀:Day(1):min(data.lastdate,data.outbreakdate+c₀+Δc);
-end
-σ = TimeArray(assimtime,ones(length(assimtime),2),[:Confirmed,:Deaths]);
-μ = TimeArray(assimtime,ones(length(assimtime),3),[:β,:γ,:δ]);
+    d₀ = data.outbreakdate + c₀
+end;
+
+assimtime = DateTime(d₀)-13*Millisecond(Δc)/10:Hour(1):DateTime(d₀)+13*Millisecond(Δc)/10;
+w = exp.(-((assimtime.-DateTime(d₀))./Millisecond(Δc)).^10);
+w[assimtime .> DateTime(data.lastdate)] .= 0.0;
+
+σ = TimeArray(assimtime,repeat(w,1,2),[:Confirmed,:Deaths]);
+μ = TimeArray(assimtime,repeat(w,1,3),[:β,:γ,:δ]);
 
 ### Initialize data assimilation
 base = Corona.Baseline(data.cases, init_u, init_p;
                 C = data.map, start = data.outbreakdate,
                 stop = timestamp(data.cases)[end] + Day(sponge),
-                step = Hour(1), σ = σ, μ = μ);
-
-### Initialize residual
-J = Vector{Union{Missing,Float64}}(missing,maxiters);
-J_ini = Corona.datanorm(base);
-J_min = Inf;
-J_argmin = 1;
+                #step = Hour(1), σ = σ, μ = μ, dataw = Hour(24));
+                step = Hour(1), σ = σ, μ = μ,
+                interptype = SteffenMonotonicInterpolation());
 
 ### Print some other information
 if coldstart
@@ -85,7 +96,15 @@ printfmt(" with tolerance {}\n",tolerance)
 printfmt("| maximum number of iterations {}\n",maxiters)
 print("└ and optimiser $opt\n\n")
 
-println("Assimilation time: $assimtime")
+println("Assimilation time: $assimtime\n")
+
+### Initialize some variables
+J = Vector{Union{Missing,Float64}}(missing,maxiters);
+J_ini = Corona.datanorm(base);
+# J_ini = 1.0;
+J_min = Inf;
+J_argmin = 1;
+i_rof = 0;
 
 ### Exectue data assimilation
 da_run = Corona.DA(base);
@@ -94,14 +113,20 @@ da_opt = da_run;
 ### Optimise data assimilation
 try
     for i=1:maxiters
-        global da_run, da_opt, J_min, J_argmin, convegence
+        global da_run, da_opt, J_min, J_argmin, convegence, i_rof
 
         da_run = Corona.apply!(opt,da_run)
         J[i] = Corona.residual(Corona.baseline(da_run))/J_ini
         Corona.extend_solution!(da_run)
 
         if J[i] < J_min
-            convegence = (log10(J_min) - log10(J[i])) / (log10(i) - log10(J_argmin))
+            # if i - i_rof >= roffreq
+            #     p = Corona.ROF(values(da_run.p), rofparam)
+            #     da_run = Corona.DA(da_run, p)
+            #     i_rof = i
+            #     println("  ... filtering")
+            # end
+            # convegence = (log10(J_min) - log10(J[i])) / (log10(i) - log10(J_argmin))
             J_min = J[i]
             J_argmin = i
             da_opt = da_run
@@ -115,7 +140,7 @@ try
             end
             print(i," ")
             printstyled(format("{:.8f}",minimum(J[iscreen]));color=color)
-            printstyled(format(" {:.8f}\n",convegence);color=:blue)
+            printstyled(format(" {:.8f}\n",J_min);color=:blue)
         end
         if J[i] < tolerance
             printstyled("\nSTOP: ";bold=true,color=:green)
@@ -133,7 +158,7 @@ catch e
 end
 
 ### Useful for plotting
-da = da_opt
+da = da_opt;
 
 ### Save data assimilation result
-Corona.save(dataconfig, region, da, data = data, interactive = true)
+Corona.save(dataconfig, region, da, data = data, interactive = true);
